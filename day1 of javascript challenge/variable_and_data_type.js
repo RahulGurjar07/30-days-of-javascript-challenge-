045a58fa31af81865960d52860406ec56b92aeed
Rahul Gurjar <99276893+RahulGurjar07@users.noreply.github.com>
2024-07-15 17:18:42 +0000

    day 1 of 30 days js challenge

---
+++ day1 of javascript challenge/variable_and_data_type.js
@@ -0,0 +1,11 @@
+// Activity 1: Variable Declaration 
+
+// Task 1: Declare a variable using var assign it a number, and log the value to the console
+
+var number = 1;
+console.log(number)
+
+// Task 2: Declare a variable using let assign it a string , and log the value to the console
+
+let assignStr = "day 1 of 30 days js challenge"
+console.log(assignStr);
